a95014d4006969c2e38e356367e2f999980ef82e
robneu <rob@wpsitecare.com>
2015-03-26 14:45:06 -0500

    Cleaned up comments templates

--- comment/comment.php
+++ comment/comment.php
@@ -13,12 +13,21 @@
 
 <li <?php hybrid_attr( 'comment' ); ?>>
 
+	<?php echo get_avatar( $comment, 48 ); ?>
+
 	<article>
 		<header class="comment-meta">
-			<?php echo get_avatar( $comment ); ?>
-			<cite <?php hybrid_attr( 'comment-author' ); ?>><?php comment_author_link(); ?></cite><br />
-			<time <?php hybrid_attr( 'comment-published' ); ?>><?php printf( __( '%s ago', 'compass' ), human_time_diff( get_comment_time( 'U' ), current_time( 'timestamp' ) ) ); ?></time>
-			<a <?php hybrid_attr( 'comment-permalink' ); ?>><?php _e( 'Permalink', 'compass' ); ?></a>
+			<cite <?php hybrid_attr( 'comment-author' ); ?>><?php comment_author_link(); ?></cite>
+			<a <?php hybrid_attr( 'comment-permalink' ); ?>>
+				<time <?php hybrid_attr( 'comment-published' ); ?>>
+					<?php
+					printf(
+						__( '%s ago', 'compass' ),
+						human_time_diff( get_comment_time( 'U' ), current_time( 'timestamp' ) )
+					);
+					?>
+				</time>
+			</a>
 			<?php edit_comment_link(); ?>
 		</header><!-- .comment-meta -->
 
@@ -26,5 +35,8 @@
 			<?php comment_text(); ?>
 		</div><!-- .comment-content -->
 
-		<?php hybrid_comment_reply_link(); ?>
+		<footer class="comment-meta">
+			<?php hybrid_comment_reply_link(); ?>
+		</footer><!-- .comment-meta -->
+
 	</article>
--- comment/error.php
+++ comment/error.php
@@ -16,14 +16,16 @@
 	<p class="comments-closed pings-open">
 		<?php
 			// Translators: The two %s are placeholders for HTML. The order can't be changed.
-			printf( __( 'Comments are closed, but %strackbacks%s and pingbacks are open.', 'compass' ), '<a href="' . esc_url( get_trackback_url() ) . '">', '</a>' );
+			printf(
+				__( 'Comments are closed, but %strackbacks%s and pingbacks are open.', 'compass' ),
+				'<a href="' . esc_url( get_trackback_url() ) . '">',
+				'</a>'
+			);
 		?>
 	</p><!-- .comments-closed .pings-open -->
 	<?php
 
-endif;
-
-if ( ! comments_open() ) : ?>
+elseif ( ! comments_open() ) : ?>
 
 	<p class="comments-closed">
 		<?php _e( 'Comments are closed.', 'compass' ); ?>
